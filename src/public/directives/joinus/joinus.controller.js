9f2631f44eb0c2bf602a4214c854ed3a351a9bae
Lawrence McDaniel <lpm0073@gmail.com>
2017-05-25 13:13:50 -0500

    added api post integration to join us form

--- src/public/directives/joinus/joinus.controller.js
+++ src/public/directives/joinus/joinus.controller.js
@@ -13,10 +13,42 @@
       return ddo;
     }
 
-
-    JoinusController.$inject = [];
-    function JoinusController() {
+    JoinusController.$inject = ['$http'];
+    function JoinusController($http) {
       console.log('JoinusController instantiated');
+
+      var ctrl = this;
+        ctrl.userInfo = {};
+        ctrl.saved = false;
+        ctrl.message = '';
+        ctrl.response = '';
+        ctrl.userInfo.comment = '';
+
+        ctrl.setContactForm = function() {
+            console.log('JoinusController.setContactForm()');
+
+            var url = "https://api-beheard.lawrencemcdaniel.com/v1/joinus/";
+            var data = '{ "firstName" : "' + ctrl.userInfo.firstName + '", "lastName" : "' + ctrl.userInfo.lastName + '", "emailAddress" : "' + ctrl.userInfo.emailAddress + '", "comment" : "' + ctrl.userInfo.comment + '" }';
+            var config = "{ 'contentType': 'application/json; charset=utf-8', 'dataType': 'json'}";
+            var promise = $http.post(url, data, config);
+
+            console.log('ctrl.userInfo', ctrl.userInfo);
+            console.log('url', url);
+            console.log('data', data);
+            console.log('config', config);
+
+            promise.then(
+              function success(response) {
+                console.log('http promise success.', response);
+                ctrl.saved = true;
+                ctrl.message = 'Thank you! Your information has been saved.';
+              },
+              function failure(response) {
+                console.log('http promise failure. something terrible happened: ', response);
+                ctrl.message = 'Something terrible has happened.';
+              });
+
+        };
 
     }
 })();
