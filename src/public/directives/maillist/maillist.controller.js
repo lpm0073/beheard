16057fc6d2858df84d4477ca77c5e939ff4e5724
Lawrence McDaniel <lpm0073@gmail.com>
2017-05-25 15:42:32 -0500

    added api integration for mail list form

--- src/public/directives/maillist/maillist.controller.js
+++ src/public/directives/maillist/maillist.controller.js
@@ -14,9 +14,36 @@
     }
 
 
-    MaillistController.$inject = [];
-    function MaillistController() {
+    MaillistController.$inject = ['$http'];
+    function MaillistController($http) {
       console.log('MaillistController instantiated');
+
+      var ctrl = this;
+        ctrl.saved = false;
+        ctrl.emailAddress = '';
+        ctrl.message = '';
+        ctrl.response = '';
+
+        ctrl.submitForm = function() {
+            console.log('MaillistController.submitForm()');
+
+            var url = "https://api-beheard.lawrencemcdaniel.com/v1/maillist/";
+            var data = '{ "emailAddress" : "' + ctrl.userInfo.emailAddress + '" }';
+            var config = "{ 'contentType': 'application/json; charset=utf-8', 'dataType': 'json'}";
+            var promise = $http.post(url, data, config);
+
+            promise.then(
+              function success(response) {
+                console.log('http promise success.', response);
+                ctrl.saved = true;
+                ctrl.message = 'Thank you! Please check your email inbox.';
+              },
+              function failure(response) {
+                console.log('http promise failure. something terrible happened: ', response);
+                ctrl.message = 'Something terrible has happened.';
+              });
+
+        };
 
     }
 })();
